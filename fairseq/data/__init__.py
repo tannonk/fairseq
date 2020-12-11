03f121f0054d75b04a9bc14d4afbb9fe75f4695a
Anastassia Shaitarova <shaita@cl.uzh.ch>
2020-12-11 04:00:14 +0100

    registered LanguageTripleDataset Class

--- fairseq/data/__init__.py
+++ fairseq/data/__init__.py
@@ -20,6 +20,7 @@ from .denoising_dataset import DenoisingDataset
 from .id_dataset import IdDataset
 from .indexed_dataset import IndexedCachedDataset, IndexedDataset, IndexedRawTextDataset, MMapIndexedDataset
 from .language_pair_dataset import LanguagePairDataset
+from .language_pair_dataset_knowledge import LanguageTripleDataset
 from .list_dataset import ListDataset
 from .lm_context_window_dataset import LMContextWindowDataset
 from .lru_cache_dataset import LRUCacheDataset
@@ -75,6 +76,7 @@ __all__ = [
     'IndexedDataset',
     'IndexedRawTextDataset',
     'LanguagePairDataset',
+    'LanguageTripleDataset',
     'LeftPadDataset',
     'ListDataset',
     'LMContextWindowDataset',
